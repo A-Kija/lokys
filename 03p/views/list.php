54836ab1d3c854dbec5ad63b2c7dc2a1168d3893
A-Kija <arvydas.kijakauskas@gmail.com>
2021-09-20 19:25:07 +0300

    list on 1

--- 03p/views/list.php
+++ 03p/views/list.php
@@ -2,22 +2,31 @@
 
 <div class="container">
     <div class="row">
-        <?php foreach($outfits as $outfit) : ?>
+        <?php $currentOutfit = '' ?>
+        <?php foreach($outfits as $index => $outfit) : ?>
+        <!-- prekes pradzia -->
+        <?php if ($currentOutfit != $outfit['color'].$outfit['type']): ?>
         <div class="col-4">
             <div class="card m-2">
                 <div class="card-body">
                     <h5 class="card-title"><?= $outfit['color'] ?> <?= $outfit['type'] ?></h5>
                     <p class="card-text">Kaina: <?= $outfit['total_price'] ?> <del><?= $outfit['price'] ?></del></p>
+        <?php endif ?>
+
                     <p class="card-text">Dydis: <?= $outfit['size'] ?></p>
                     <p class="card-text">Kiekis: <?= $outfit['amount'] ?></p>
+        
+        <?php if (!isset($outfits[$index + 1]) || $outfits[$index + 1]['color'].$outfits[$index + 1]['type'] != $outfit['color'].$outfit['type']): ?> 
                     <form action="<?= URL. 'pirkti' ?>" method="post">
                     <button class="btn btn-primary" name="id" value="<?= $outfit['id'] ?>">Pirkti</button>
                     <input type="text" style="width:30px;" name="count">
                     </form>
-                    
                 </div>
             </div>
         </div>
+        <?php endif ?>
+        <?php $currentOutfit = $outfit['color'].$outfit['type'] ?>
+        <!-- prekes pabaiga -->
         <?php endforeach ?>
     </div>
 </div>
