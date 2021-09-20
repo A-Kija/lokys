cd11e7738e0063ea83fe31db0e1450bf04c7f9a3
A-Kija <arvydas.kijakauskas@gmail.com>
2021-09-20 19:33:35 +0300

    su select

--- 03p/views/list.php
+++ 03p/views/list.php
@@ -11,15 +11,17 @@
                 <div class="card-body">
                     <h5 class="card-title"><?= $outfit['color'] ?> <?= $outfit['type'] ?></h5>
                     <p class="card-text">Kaina: <?= $outfit['total_price'] ?> <del><?= $outfit['price'] ?></del></p>
-        <?php endif ?>
-
-                    <p class="card-text">Dydis: <?= $outfit['size'] ?></p>
-                    <p class="card-text">Kiekis: <?= $outfit['amount'] ?></p>
-        
-        <?php if (!isset($outfits[$index + 1]) || $outfits[$index + 1]['color'].$outfits[$index + 1]['type'] != $outfit['color'].$outfit['type']): ?> 
+                    <div class="form-group">
+                        <select name="product" class="form-control">
+                            <?php endif ?>
+                            <option value="<?= $outfit['size'] ?>"><?= $outfit['size'] ?> Liko: <?= $outfit['amount'] ?>
+                            </option>
+                            <?php if (!isset($outfits[$index + 1]) || $outfits[$index + 1]['color'].$outfits[$index + 1]['type'] != $outfit['color'].$outfit['type']): ?>
+                        </select>
+                    </div>
                     <form action="<?= URL. 'pirkti' ?>" method="post">
-                    <button class="btn btn-primary" name="id" value="<?= $outfit['id'] ?>">Pirkti</button>
-                    <input type="text" style="width:30px;" name="count">
+                        <button class="btn btn-primary" name="id" value="<?= $outfit['id'] ?>">Pirkti</button>
+                        <input type="text" style="width:30px;" name="count">
                     </form>
                 </div>
             </div>
